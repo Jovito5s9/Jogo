94902b9a6dc6b1212fccb30b4e09a8ea26ca1588
Jovito5s9 <sjovito70@gmail.com>
2025-10-01 15:46:35 -0300

    adicionando configuração via json pra ter ou nao joystick

--- game.py
+++ game.py
@@ -3,13 +3,31 @@ from kivy.clock import Clock
 from core.player import BasicEnt
 from core.world import World
 from utils.joystick import Joystick 
+import json
 
 class Interface(FloatLayout):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
         self.size_hint = None, None
+        self.configs=[]
+        self.configuracoes()
+
+        if not self.configs["teclado"]:
+            self.add_joytick()
+    
+    def add_joytick(self):
         self.joystick = Joystick(size_hint=(None, None), size=(400, 400), pos_hint={'center_x': 2, 'center_y': 2})
         self.add_widget(self.joystick)
+    
+    def configuracoes(self):
+        try:
+            with open("configuracoes.json","r",encoding="utf-8") as config:
+                self.configs=json.load(config)
+        except:
+            with open("configuracoes.json","w",encoding="utf-8") as config:
+                newconfig={"teclado": False}
+                json.dump(newconfig, config)
+
 
 class Game(FloatLayout):
     def __init__(self,**kwargs):
