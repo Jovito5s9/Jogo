ca060b2a79f49ea846a54fa3785781d2dc0399c3
Jovito5s9 <sjovito70@gmail.com>
2025-10-23 20:00:28 -0300

    adicionando itens minerais, que posteriormente servirão para fortalecer a batata

--- saved/itens_db.py
+++ saved/itens_db.py
@@ -4,5 +4,23 @@ ITENS = {
         "tipo" : "material",
         "descrição" : "Um pouco de matéria orgânica.",
         "source" : "assets/itens/carne.png"
+    },
+    "apatita" : {
+        "raridade" : "incomum",
+        "tipo" : "material, consumível",
+        "descrição" : "Um mineral que aparenta aumentar\n a resistência e força.",
+        "source" : "assets/itens/apatita.png"
+    },
+    "mica" : {
+        "raridade" : "comum",
+        "tipo" : "material, consumível",
+        "descrição" : "Um mineral que aparenta aumentar\n a circulação de açúcares.",
+        "source" : "assets/itens/mica.png"
+    },
+    "cristal vital" : {
+        "raridade" : "raro",
+        "tipo" : "consumível",
+        "descrição" : "Um pouco de matéria orgânica.",
+        "source" : "assets/itens/cristal_vital.png"
     }
 }
