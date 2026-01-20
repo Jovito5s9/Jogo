51c1d91de88646b0b7b020414a3697c5ff2965b0
Jovito5s9 <sjovito70@gmail.com>
2026-01-20 10:43:17 -0300

    feat: adicionando dados dos equipaveis no banco de dados de itens

--- saved/itens_db.py
+++ saved/itens_db.py
@@ -30,6 +30,26 @@ ITENS = {
 
     "equipaveis":
     {
-        "chips vao ficar aqui depois":None
+        "núcleo ceifador de energia":{
+            "raridade" : "raro",
+            "tipo" : "BitCore",
+            "skill": "vampirismo",
+            "descrição": "Um chip que converte a energia liberada \nno impacto em bioenergia, realimentando os sistemas vitais.",
+            "source" : "assets/itens/BitCore/energy_reaper.png"
+        },
+        "núcleo do instinto de pânico":{
+            "raridade" : "raro",
+            "tipo" : "BitCore",
+            "skill": "panico",
+            "descrição": "Um chip neural que intercepta sinais de dor\n e os converte em impulsos motores acelerados.",
+            "source" : "assets/itens/BitCore/panic_instinct.png"
+        },
+        "núcleo da esquiva aleatória":{
+            "raridade" : "raro",
+            "tipo" : "BitCore",
+            "skill": "esguio",
+            "descrição": "Um núcleo neural instável que ativa reflexos\n evasivos de forma imprevisível.",
+            "source" : "assets/itens/BitCore/random_dodge.png"
+        }
     }
 }
