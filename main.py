c823b4dcce2d5e714c34452acf100a7f04042047
Jovito5s9 <sjovito70@gmail.com>
2026-01-26 21:15:25 -0300

    feat: agora no desktop ao abrir o jogo ele fica automaticamente em tela cheia

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+from kivy.config import Config
+
+Config.set('graphics', 'fullscreen', 'auto')
+Config.set('graphics', 'borderless', '1')
+Config.set('graphics', 'resizable', '0')
+
 from kivy.app import App 
 from game import GameScreenManager 
 from kivy.config import Config
