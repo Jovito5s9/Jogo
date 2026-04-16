5821cfdf8cd4d0159284e51f4c38a09f05d0ad72
Jovito5s9 <sjovito70@gmail.com>
2026-04-16 10:53:52 -0300

    splash screen apresentando o studio SolarBurn

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 from screens.menu_screen import MenuScreen
 from screens.game_screen import GameScreen
 from screens.config_screen import ConfiguracoesScreen
+from screens.splash_screen import SplashScreen
 from kivy.uix.screenmanager import ScreenManager, NoTransition
 
 GameScreenManager=ScreenManager(transition=NoTransition())
 
+GameScreenManager.add_widget(SplashScreen(name='splash', GameScreenManager=GameScreenManager))
 GameScreenManager.add_widget(MenuScreen(name='menu', GameScreenManager=GameScreenManager))
 GameScreenManager.add_widget(ConfiguracoesScreen(name='configurações', GameScreenManager=GameScreenManager))
 GameScreenManager.add_widget(GameScreen(name='game',GameScreenManager=GameScreenManager))
